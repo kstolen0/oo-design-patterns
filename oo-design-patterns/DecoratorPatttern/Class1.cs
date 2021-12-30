bb216da73272970e8d9ba0e6a70699e9b0b4dca6
Kristian Stolen <kstolen0@our.ecu.edu.au>
2021-12-30 12:23:46 +0800

    Create class library and Test project.

---
+++ oo-design-patterns/DecoratorPatttern/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DecoratorPatttern
+{
+    public class Class1
+    {
+        public Class1()
+        {
+            Console.WriteLine("Hello World");
+        }
+    }
+}
---
+++ oo-design-patterns/Pattern.UnitTests/DecoratorPatternTests.cs
@@ -0,0 +1,15 @@
+using DecoratorPatttern;
+using System;
+using Xunit;
+
+namespace Pattern.UnitTests
+{
+    public class DecoratorPatternTests
+    {
+        [Fact]
+        public void ItCreatesADecorator()
+        {
+            Class1 sut = new Class1();
+        }
+    }
+}
