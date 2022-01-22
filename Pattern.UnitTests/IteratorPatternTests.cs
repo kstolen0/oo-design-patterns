b7d97283c306bcf5b409f5bcbdeaf0cef485fe6b
Kristian Stolen <kstolen0@our.ecu.edu.au>
2022-01-22 10:08:44 +0800

    Create enumerable class of menu items with internal list collection.

--- Pattern.UnitTests/IteratorPatternTests.cs
+++ Pattern.UnitTests/IteratorPatternTests.cs
@@ -25,6 +25,24 @@ namespace UnitTests
             EnumerateThroughMenu(sut);
         }
 
+        [Fact]
+        public void LunchMenu_ContainsListOfMenuItems()
+        {
+            var sut = new LunchMenu();
+
+            var menuItems = sut.Menu;
+
+            menuItems.Should().BeOfType<List<MenuItem>>();
+        }
+
+        [Fact]
+        public void CanEnumerateThroughLunchMenu()
+        {
+            var sut = new LunchMenu();
+
+            EnumerateThroughMenu(sut);
+        }
+
         private void EnumerateThroughMenu(IEnumerable<MenuItem> menu)
         {
             foreach (var item in menu)
---
+++ Patterns/IteratorPattern/LunchMenu.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace Patterns.IteratorPattern
+{
+    public class LunchMenu : IEnumerable<MenuItem>
+    {
+        public IList<MenuItem> Menu;
+
+        public LunchMenu()
+        {
+            Menu = new List<MenuItem>();
+        }
+
+        public IEnumerator<MenuItem> GetEnumerator()
+        {
+            return Menu.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return (IEnumerator)GetEnumerator();
+        }
+    }
+}
