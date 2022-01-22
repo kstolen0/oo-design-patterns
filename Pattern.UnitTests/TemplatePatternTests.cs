c9d0301cee0eaf3e113ae83fcb04266440a89952
Kristian Stolen <kstolen0@our.ecu.edu.au>
2022-01-22 09:06:26 +0800

    remove reflection tests.

--- Pattern.UnitTests/TemplatePatternTests.cs
+++ Pattern.UnitTests/TemplatePatternTests.cs
@@ -2,16 +2,12 @@
 using Patterns.TemplatePattern;
 using Patterns.TemplatePattern.SubclassingMethod;
 using System;
-using System.Reflection;
 using Xunit;
 
 namespace UnitTests
 {
     public class TemplatePatternTests
     {
-
-        private string _namespace = "Patterns.TemplatePattern.SubclassingMethod.";
-
         [Fact]
         public void SortAnArrayOfMyCustomObject()
         {
@@ -40,43 +36,6 @@ namespace UnitTests
             ParentClass sut = new ChildClassB();
 
             sut.Algorithm().Should().Be(4);
-        }
-
-        [Fact]
-        public void ParentClass_Defines_NonAbstractNonVirtualAlgorithm()
-        {
-            var parentClass = Assembly.GetAssembly(typeof(ParentClass)).GetType(_namespace + nameof(ParentClass));
-
-            var algorithmMethod = parentClass.GetMethod(nameof(ParentClass.Algorithm));
-
-            algorithmMethod.IsAbstract.Should().BeFalse();
-            algorithmMethod.IsVirtual.Should().BeFalse();
-        }
-
-        [Fact]
-        public void ParentClass_Defines_AbstractStepB()
-        {
-            var parentClass = Assembly.GetAssembly(typeof(ParentClass)).GetType(_namespace + nameof(ParentClass));
-
-            var stepBMethod = parentClass.GetMethod(nameof(ParentClass.StepB));
-
-            stepBMethod.IsAbstract.Should().BeTrue();
-        }
-
-        [Fact]
-        public void ChildClassA_InheritsFrom_ParentClass()
-        {
-            var childClass = Assembly.GetAssembly(typeof(ChildClassA)).GetType(_namespace + nameof(ChildClassA));
-
-            childClass.BaseType.Name.Should().Be(nameof(ParentClass));
-        }
-
-        [Fact]
-        public void ChildClassB_InheritsFrom_ParentClass()
-        {
-            var childClass = Assembly.GetAssembly(typeof(ChildClassB)).GetType(_namespace + nameof(ChildClassB));
-
-            childClass.BaseType.Name.Should().Be(nameof(ParentClass));
         }
     }
 }
