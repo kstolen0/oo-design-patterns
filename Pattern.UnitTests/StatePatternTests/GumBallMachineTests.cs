38ae477894b03bab6c4e4686efe2fa0a36196cdf
Kristian Stolen <kstolen0@our.ecu.edu.au>
2022-01-27 11:13:05 +0800

    Add public gum ball machine

---
+++ Pattern.UnitTests/StatePatternTests/GumBallMachineTests.cs
@@ -0,0 +1,67 @@
+﻿using FluentAssertions;
+using Patterns.StatePattern;
+using Xunit;
+
+namespace UnitTests.StatePatternTests
+{
+    public class GumBallMachineTests
+    {
+        [Fact]
+        public void TurnCrank_ReturnsFalse_WhenCoinHasNotBeenInserted()
+        {
+            IGumBallMachine sut = new GumBallMachine(1);
+
+            var result = sut.TurnCrank();
+
+            result.Should().BeFalse();
+        }
+
+        [Fact]
+        public void TurnCrank_ReturnsTrue_WhenCoinIsFirstInserted()
+        {
+            IGumBallMachine sut = new GumBallMachine(1);
+
+            sut.InsertCoin();
+            var result = sut.TurnCrank();
+
+            result.Should().BeTrue();
+        }
+
+        [Fact]
+        public void TurnCrank_ReturnsFalse_WhenEmpty()
+        {
+            IGumBallMachine sut = new GumBallMachine(1);
+            var result = false;
+
+            sut.InsertCoin();
+            result = sut.TurnCrank();
+            sut.InsertCoin();
+            result = sut.TurnCrank();
+
+            result.Should().BeFalse();
+        }
+
+        [Fact]
+        public void GumBallMachine_IsEmpty_WithDefaultConstructor()
+        {
+            IGumBallMachine sut = new GumBallMachine();
+
+            sut.InsertCoin();
+            var result = sut.TurnCrank();
+
+            result.Should().BeFalse();
+        }
+
+        [Fact]
+        public void Refill_RefillsAnEmptyGumBallMachine()
+        {
+            IGumBallMachine sut = new GumBallMachine();
+
+            sut.Refill(1);
+            sut.InsertCoin();
+            var result = sut.TurnCrank();
+
+            result.Should().BeTrue();
+        }
+    }
+}
--- Patterns/StatePattern/GumBallMachine.cs
+++ Patterns/StatePattern/GumBallMachine.cs
@@ -1,7 +1,30 @@
 ﻿namespace Patterns.StatePattern
 {
-    public class GumBallMachine
+    public class GumBallMachine : IGumBallMachine
     {
+        IInternalGumBallMachine _machine;
 
+        public GumBallMachine() : this(0)
+        { }
+
+        public GumBallMachine(int gumballs)
+        {
+            _machine = new InternalGumBallMachine(gumballs);
+        }
+
+        public void InsertCoin()
+        {
+            _machine.InsertCoin();
+        }
+
+        public void Refill(int gumBalls)
+        {
+            _machine.Refill(gumBalls);
+        }
+
+        public bool TurnCrank()
+        {
+            return _machine.TurnCrank();
+        }
     }
 }
